e242893f8c825c8d7fe0e1e57aef7ad5973f28c6
Jspaezp <jspaezp@gmail.com>
2020-05-28 11:03:27 -0400

    added more required packages and improved script readability

--- install_sway.bash
+++ install_sway.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+space () { for i in {1..5} ; do echo "" ; done } 
 
 # Makes running the script print what is being run and stops running after
 # the first error
@@ -11,16 +12,43 @@ dnf install waybar -y
 dnf install sway waybar kitty -y
 dnf install neofetch -y
 
+dnf install swaylock pavucontrol fontawesome-fonts pcmanfm wofi -y
+
 foldername=$(date +%Y%m%d%_H%M)
 backupname="~/.config/bkp_${foldername}"
 mkdir -p "${backupname}"
 
+space
 
 for configdir in ~/.config/sway ~/.config/kitty ~/.config/waybar; do
   if [ -d "${configdir}"]; then
     mv -v "${configdir}" "${backupname}/."
   fi
 done
+
+space
+
+for i in $(ls ./config/ ) ; do
+  ln -s "${PWD}/config/${i}" "~/.config/."
+done
+
+space
+
+for i in $(ls ./config/ ) ; do
+  head "~/.config/${i}/"*
+done
+
+space
+
+sway --version
+waybar --version
+kitty --version
+
+space 
+
+neofetch
+
+space
 
 # Remove the temporary folder if it is empty, else let the user know where it is
 if [ -z "$(ls -A ${backupname} )" ]; then
@@ -29,16 +57,3 @@ else
   echo "Saved the former conflicting dotfiles in: "
   echo "----->>>>>>>>" "${backupname}"
 fi
-
-for i in $(ls ./config/ ) ; do
-  ln -s "${PWD}/config/${i}" "~/.config/."
-done
-
-for i in $(ls ./config/ ) ; do
-  head "~/.config/${i}/"*
-done
-
-sway --version
-waybar --version
-kitty --version
-neofetch
